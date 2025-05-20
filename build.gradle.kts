715f64c43286775d266c2d64a74b4be4d509e578
theProject - Tristan Smith <tjsmith@bytheproject.com>
2025-05-20 15:56:38 -0400

    just got started

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+// build.gradle.kts (Project level)
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Safe args plugin for navigation
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory) // Corrected line
+}
